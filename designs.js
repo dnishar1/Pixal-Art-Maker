ce7abb1fa9bc556fb283d1d6b421e66b073575c8
Dhairya Nishar <dhairyatnishar@gmail.com>
2019-07-12 15:29:47 -0400

    Initial commit

---
+++ designs.js
@@ -0,0 +1,32 @@
+const element = document.getElementById('pixelCanvas')
+let color = $("#colorPicker"); // Select color input
+let inputHeight = $("#inputHeight");
+let inputWidth = $("#inputWidth");
+
+$('#submit').click(function(inputSubmit){
+    inputSubmit.preventDefault();
+    makeGrid();
+});
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+    element.innerHTML = '';
+    let h = inputHeight.val();
+    let w = inputWidth.val();
+    let events = function(pixelArt){
+        pixelArt.addEventListener('click', function(){
+            pixelArt.style.backgroundColor = color.val();
+        });
+    }
+
+    for (let i = 0; i < h; i++) {
+        let r = element.insertRow(i);
+        for (let j = 0; j < w; j++) {
+            let pixelArt = r.insertCell(j);
+            pixelArt.addEventListener('click', events(pixelArt));
+        }
+    }
+
+}
